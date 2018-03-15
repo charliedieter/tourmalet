ed5d307a5a66a3aaf9bfcddf41bd79031aacd9bf
Charlie Dieter <charlie.dieter@gmail.com>
2018-03-15 19:59:19 -0400

    add to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([
+{ username: 'Fred',email: "fred@gmail.com",
+password: "password"
+},{ username: 'Charlie',email: "charlie@gmail.com",
+password: "password"
+},{ username: 'Conner',email: "conner@gmail.com",
+password: "password"
+},{ username: 'Laurette',email: "user2@gmail.com",
+password: "password"
+},{ username: 'Zach',email: "user3@gmail.com",
+password: "password"
+},{ username: 'Aislinn',email: "email4@gmail.com",
+password: "password"
+},{ username: 'Guest',email: "email@gmail.com",
+password: "password"
+}])
+
+activities = Activity.create([{title: "HI mom ",
+  description: "",
+  polyline: "ufxwFppobMlJ_Z`GmRNe@OIkBsAcG{DeG_EsAaAkAs@sE{C{BwAwJsG_HsE",
+  athlete_id: 3, distance: "1.17",
+  est_moving_time: "24:41",
+  type_of: "Run"}])
